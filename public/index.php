361efe0420c93b421009e0f7bb48cf94ff6ac643
MireiaCastillejo <ls31321@salleurl.edu>
2019-04-04 22:26:32 +0200

    Carpetas

--- public/index.php
+++ public/index.php
@@ -4,12 +4,12 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$settings = require_once __DIR__ . '/../config/settings.php';
+$settings = require_once __DIR__ . '/../app/settings.php';
 
 $app = new \Slim\App($settings);
 
-require_once __DIR__ . '/../config/routes.php';
+require_once __DIR__ . '/../app/routes.php';
 
-require_once __DIR__ . '/../config/dependencies.php';
+require_once __DIR__ . '/../app/dependencies.php';
 
 $app->run();
